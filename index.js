c5270081c64c8c08cc1f54725e36aee40d7429c5
wirednkod <wirednkod@gmail.com>
2023-05-04 16:21:59 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,65 @@
+import "regenerator-runtime/runtime";
+import { createScClient, WellKnownChain } from "@substrate/connect";
+import UI from "./view";
+const getStruct = (name) => ({
+    auth: document.getElementById(name + "Auth"),
+    spec: document.getElementById(name + "Spec"),
+    trans: document.getElementById(name + "Trans"),
+    newBlock: document.getElementById(name + "New"),
+    best: document.getElementById(name + "Best"),
+    finalized: document.getElementById(name + "Finalized"),
+});
+window.onload = () => {
+    const loadTime = performance.now();
+    const ui = new UI({ containerId: "messages" }, { loadTime });
+    ui.showSyncing();
+    const west = getStruct("westend");
+    const westmint = getStruct("westmint");
+    const showStuffInUI = (who, what) => {
+        const json = JSON.parse(what);
+        switch (json.event) {
+            case "initialized":
+                if (who.auth)
+                    who.auth.innerText = JSON.stringify(json.finalizedBlockRuntime.spec.authoringVersion);
+                if (who.spec)
+                    who.spec.innerText = JSON.stringify(json.finalizedBlockRuntime.spec.specVersion);
+                if (who.trans)
+                    who.trans.innerText = JSON.stringify(json.finalizedBlockRuntime.spec.transactionVersion);
+                break;
+            case "newBlock":
+                if (who.newBlock)
+                    who.newBlock.innerText = JSON.stringify(json.blockHash);
+                break;
+            case "bestBlockChanged":
+                if (who.best)
+                    who.best.innerText = JSON.stringify(json.bestBlockHash);
+                break;
+            case "finalized":
+                if (who.finalized) {
+                    who.finalized.innerText = JSON.stringify(json.finalizedBlockHashes.join());
+                }
+                break;
+        }
+    };
+    void (async () => {
+        try {
+            const scClient = createScClient();
+            // const scClient = createScClient({
+            //   forceEmbeddedNode: true,
+            //   embeddedNodeConfig: {
+            //     maxLogLevel: 4
+            //   }
+            // })
+            const mainChain = await scClient.addWellKnownChain(WellKnownChain.polkadot, function jsonRpcCallback(response) {
+                if (!(JSON.parse(response).params?.result)) {
+                    return;
+                }
+                showStuffInUI(west, JSON.stringify(JSON.parse(response).params.result));
+            });
+            mainChain.sendJsonRpc('{"jsonrpc":"2.0","id":"1","method":"chainHead_unstable_follow","params":[true]}');
+        }
+        catch (error) {
+            ui.error(error);
+        }
+    })();
+};
